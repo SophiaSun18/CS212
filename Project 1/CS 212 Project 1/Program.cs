8f8f4191b8f2afc0fd80738445bfec519f21c53d
sza <sza@dhcp100-229.calvin.edu>
2022-09-16 16:59:48 -0400

    prject 1

---
+++ Project 1/CS 212 Project 1/Program.cs
@@ -0,0 +1,40 @@
+﻿/*********************************************************************
+* Floor(lg lg n) calculator program using C#.
+*
+* Sophia Sun, 9/2022
+* *********************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Project_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* The console interface of the program. */
+            Console.WriteLine("Floor(lg lg n) computer!");
+            while (true)
+            {
+                Console.Write("Enter N: ");
+                int n = int.Parse(Console.ReadLine());
+                int floor = Logn(Logn(n));
+                Console.WriteLine("The value of Floor(lg lg {0})) is {1}.", n, floor);
+            }
+        }
+        static int Logn(int n)
+        {
+            /* Define a new function calculating lg(n) using a simple while loop. */
+            /* Call this function recursively to get the value of floor(lg lg n). */
+            int i = 0;
+            while (n > 1)
+            {
+                n = (int)Math.Floor((decimal) n / 2);
+                i++;
+            }
+            return i;
+        }
+    }
+}
---
+++ Project 1/README.md
@@ -0,0 +1,3 @@
+CS 212 Project 1
+
+* This is a Floor(lg lg n) calculator program written in C#.
